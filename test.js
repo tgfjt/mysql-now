83b2b57d09a4332c088a7bcf219fe43c6097863a
tgfjt <tgfjt.mail@gmail.com>
2017-06-14 19:00:09 +0900

    tweaks test message

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ const tape = require('tape');
 
 const now = require('./');
 
-tape('should render on the server', function(t) {
+tape('should formatted', function(t) {
   const d = new Date(Date.UTC(2017, 5, 14, 9));
 
   t.equal(now(d), '2017-06-14 09:00:00', 'result was OK');
